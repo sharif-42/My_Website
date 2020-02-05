558ab8713e0e264f5e112a8563dbdb11986dc5aa
sharif-42 <sharifulcsehstu@gmail.com>
2020-02-06 00:58:28 +0600

    all templates added

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from users import views
+
 urlpatterns = [
-
-
+    path('home/', views.Home.as_view(), name='home'),
+    # path('home/', views.Home.as_view(), name='home'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class Home(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'index.html'
+
+    def get(self, request):
+        return Response({}, status=status.HTTP_200_OK)
